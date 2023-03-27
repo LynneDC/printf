3b6367b3f2d79ef15e2e3f0012b8efb43fd1bf85
LynneDC <roselinedc1308@gmail.com>
2023-03-27 12:50:26 -0700

    updated

---
+++ function1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+* _printf - prints formatted output to stdout
+* @format: a string containing zero or more directives
+*
+* Return: the number of characters printed
+*/
+int _printf(const char *format, ...)
+{
+va_list args;
+int count = 0;
+
+va_start(args, format);
+
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'd' || *format == 'i')
+{
+int num = va_arg(args, int);
+printf("%d", num);
+count++;
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+
+va_end(args);
+
+return (count);
+}
+
